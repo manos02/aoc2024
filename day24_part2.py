c766fd63c5f93fda06801349f965719820dbd765
manos02 <64806766+manos02@users.noreply.github.com>
2024-12-25 17:27:37 +0200

    day 24 part 2

--- day24_part2.py
+++ day24_part2.py
@@ -1,4 +1,3 @@
-from collections import deque
 
 f = open("input.txt", "r")
 f = f.read().strip()
@@ -11,7 +10,7 @@ for l in a.split("\n"):
     gate, value = l.split(":")
     wire_dict[gate] = int(value)
 
-# print(wire_dict)
+
 x = ''
 y = ''
 for k,v in wire_dict.items():
@@ -32,74 +31,56 @@ for l in b.split("\n"):
     instructions.append((g1,g2,operator,g3))
 
 
-
-print(instructions)
-
-
 def find(operand, xi, yi):
+    print(operand, xi, yi)
     for inst in instructions:
-        if inst[2] == operand and (inst[0] == xi or inst[0] == yi) and  (inst[1] == xi or inst[1] == yi):
+        if inst[2] == operand and (inst[0] == xi or inst[0] == yi) and (inst[1] == xi or inst[1] == yi):
             return inst[3]
         
     return None
 
 
+c0 = None
+swapped = []
 
 for i in range(45):
-    i = bin(6)[2:]
+    i = str(i).zfill(2)
+    xor_res, and_res, r1, z1, c1 = None, None, None, None, None
 
-    and_res = find("XOR", f"x{i}", f"y{i}")
+    xor_res = find("XOR", f"x{i}", f"y{i}")
     and_res = find("AND", f"x{i}", f"y{i}")
-    
+
+    print(i, xor_res, and_res, c0)
+    if c0:
+        r1 = find("AND", c0, xor_res)
+        if not r1:
+            xor_res, and_res = and_res, xor_res
+            swapped.extend([xor_res, and_res])
+            r1 = find("AND", c0, xor_res)
+
+        z1 = find("XOR", c0, xor_res)
 
 
+        if xor_res and xor_res.startswith("z"):
+            xor_res, z1 = z1, xor_res
+            swapped.extend([xor_res, z1])
+
+        if and_res and and_res.startswith("z"):
+            and_res, z1 = z1, and_res
+            swapped.extend([and_res, z1])
+
+        if r1 and r1.startswith("z"):
+            r1, z1 = z1, r1
+            swapped.extend([r1, z1])
+
+        c1 = find("OR", r1, and_res)
+
+    if c1 and c1.startswith("z") and c1 != "z45":
+        c1, z1 = z1, c1
+        swapped.extend([c1, z1])
 
 
+    c0 = c1 if c0 else and_res
 
-
-
-
-
-# def solve(instructions):
-#     flag = True
-#     while flag:
-#         flag = False
-#         for g1,g2,operator, g3 in instructions:        
-#             if g1 not in wire_dict or g2 not in wire_dict:
-#                 flag = True
-#                 continue
-#             val1 = wire_dict[g1]
-#             val2 = wire_dict[g2]
-#             if operator == 'XOR':
-#                 res = val1 ^ val2
-#             elif operator == 'AND':
-#                 res = val1 & val2
-#             else:
-#                 res = val1 | val2
-#             wire_dict[g3] = res
-
-
-# solve(instructions)
-
-# sorted_dict = {key: value for key, value in sorted(wire_dict.items())}
-# res = ""
-
-# for k,v in sorted_dict.items():
-#     if k[0] == "z":
-#         res += str(v)
-
-
-
-# wrong_res = bin(int(res[::-1], 2))
-# correct_res = bin(int(x, 2) + int(y, 2))
-
-
-
-# l = {}
-# for i in range(0, len(correct_res)-2):
-#     if correct_res[::-1][i] != wrong_res[::-1][i]:
-#         l[i] = correct_res[::-1][i]
-        
-
-
+print(",".join(sorted(swapped)))
 
